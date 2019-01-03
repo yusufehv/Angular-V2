0a445e3d0c6d18cc7ec80ee9f1953a7eb3d6aa07
yusufehv <yusufyildizaga@hotmail.com>
2019-01-03 07:06:31 +0100

    3-1-2019

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,6 +11,8 @@ import { DetailsComponent } from './details/details.component';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavComponent } from './nav/nav.component';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +21,9 @@ import { NavComponent } from './nav/nav.component';
     PostsComponent,
     UsersComponent,
     DetailsComponent,
-    NavComponent
+    NavComponent,
+    LoginFormComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
